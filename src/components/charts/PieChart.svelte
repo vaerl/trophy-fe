6ccd8e2702f19f07b362b09204679f9f0cd75454
vaerl <me@lukas.run>
2024-03-25 00:03:43 +0100

    Use a instead of goto.

--- src/components/charts/PieChart.svelte
+++ src/components/charts/PieChart.svelte
@@ -1,8 +1,6 @@
 <script lang="ts">
 	// copied from here: https://github.com/GeekyAnts/svelte-admin-dashboard/blob/master/src/components/Charts/DoughnutChart.svelte
 	import { onMount } from 'svelte';
-	// this has to be done like this: https://stackoverflow.com/questions/71030372/does-not-provide-an-export-named-tooltip-even-though-i-can-output-tooltip
-	import { goto } from '$app/navigation';
 	import Chart, {
 		ArcElement,
 		CategoryScale,
@@ -44,20 +42,22 @@
 	}
 </script>
 
-{#if total == 0}
-	<h1 class="text-lg font-bold text-center cursor-pointer" on:click={() => goto(link)}>
-		Momentan sind keine <br />
-		{name} angelegt.
-	</h1>
-{:else}
-	<div class="relative cursor-pointer" on:click={() => goto(link)}>
-		<h4 class="text-3xl text-center pb-4 font-bold">{name}</h4>
-		<canvas id={'doughnut-chart' + name} />
-		<div class="absolute top-1/2 w-full">
-			<p class="text-center w-full font-extrabold stat-value">
-				{total}
-			</p>
-			<p class="text-center w-full stat-desc">{name} insgesamt</p>
+<a href={link}>
+	{#if total == 0}
+		<h1 class="text-lg font-bold text-center cursor-pointer">
+			Momentan sind keine <br />
+			{name} angelegt.
+		</h1>
+	{:else}
+		<div class="relative cursor-pointer">
+			<h4 class="text-3xl text-center pb-4 font-bold">{name}</h4>
+			<canvas id={'doughnut-chart' + name} />
+			<div class="absolute top-1/2 w-full">
+				<p class="text-center w-full font-extrabold stat-value">
+					{total}
+				</p>
+				<p class="text-center w-full stat-desc">{name} insgesamt</p>
+			</div>
 		</div>
-	</div>
-{/if}
+	{/if}</a
+>
