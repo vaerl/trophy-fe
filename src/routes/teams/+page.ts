d225968d8de3f555ad576a866e3b6fe7fc66a97e
vaerl <me@lukas.run>
2024-03-25 00:03:47 +0100

    Add load.

---
+++ src/routes/teams/+page.ts
@@ -0,0 +1,12 @@
+import type { Game, Team } from '$lib/model';
+
+export async function load({ fetch, params }) {
+	const baseUrl: string = import.meta.env.VITE_BACKEND_URL;
+
+	const teamsRes = await fetch(`${baseUrl}/teams`, { credentials: 'include' });
+	const teams: Team[] = await teamsRes.json();
+
+	return {
+		teams
+	};
+}
