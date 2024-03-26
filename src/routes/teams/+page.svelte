8a1437b5a3ba323c4c7a65a95b793e1ab6f4e4ab
vaerl <me@lukas.run>
2024-03-26 23:25:20 +0100

    Implement teams-table.

--- src/routes/teams/+page.svelte
+++ src/routes/teams/+page.svelte
@@ -1,32 +1,63 @@
 <script lang="ts">
-	import { pendingTeams, teams } from '../../lib/stores';
-	import Table from '../../components/table/Table.svelte';
-	import Breadcrumbs from '../../components/Breadcrumbs.svelte';
+	import { goto } from '$app/navigation';
+	import Plus from '../../components/icons/Plus.svelte';
+	import { DataHandler, Datatable, Th, ThFilter } from '@vincjo/datatables';
 
 	export let data;
-
-	let columns = [
-		{ id: 'trophy_id', name: 'Trophy-ID' },
-		{ id: 'name', name: 'Name' },
-		{ id: 'gender', name: 'Gender' },
-		{ id: 'points', name: 'Punkte' }
-	];
-
-	teams.get('teams');
-	pendingTeams.get('teams/pending');
-
-	$: teamsLoading = teams.loading;
-	$: pendingTeamsLoading = pendingTeams.loading;
-	// TODO simplify this
+	const handler = new DataHandler(data.teams, { rowsPerPage: 10 });
+	const rows = handler.getRows();
 </script>
 
-<Breadcrumbs current="Teams" paths={[]} />
+<h1 class="absolute-center-x left-1/2 text-4xl font-bold pt-6">Teams</h1>
 
-<Table
-	type="team"
-	items={teams}
-	itemsLoading={teamsLoading}
-	pendingItems={pendingTeams}
-	pendingItemsLoading={pendingTeamsLoading}
-	{columns}
-/>
+<a
+	href="/teams/create"
+	class="cursor-pointer btn btn-circle btn-neutral absolute bottom-14 right-4 z-10"
+>
+	<Plus />
+</a>
+<Datatable {handler}>
+	<table class="table table-zebra">
+		<thead>
+			<tr>
+				<Th {handler} orderBy="trophy_id">Trophy-ID</Th>
+				<Th {handler} orderBy="name">Name</Th>
+				<Th {handler} orderBy="gender">Typ</Th>
+				<Th {handler} orderBy="points">Punkte</Th>
+			</tr>
+			<tr>
+				<ThFilter {handler} filterBy="trophy_id" />
+				<ThFilter {handler} filterBy="name" />
+				<ThFilter {handler} filterBy="gender" />
+				<ThFilter {handler} filterBy="points" />
+			</tr>
+		</thead>
+
+		<tbody>
+			{#each $rows as row}
+				<!-- even though I'd like to use <a>, only this will make sure the entire row is clickable -->
+				<tr on:click={() => goto(`/teams/${row.id}`)} class="cursor-pointer">
+					<td>
+						{row.trophy_id}
+					</td>
+					<td>{row.name}</td>
+					<td>{row.gender}</td>
+					<td>{row.points}</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+</Datatable>
+
+<style>
+	tbody td {
+		border: 1px solid #f5f5f5;
+		padding: 4px 20px;
+	}
+	tbody tr {
+		transition: all, 0.2s;
+	}
+	tbody tr:hover {
+		background: #f5f5f5;
+	}
+</style>
