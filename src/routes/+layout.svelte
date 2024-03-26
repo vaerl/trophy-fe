754e621f5ccfc6a16a1db800f8e19eea928b9316
vaerl <me@lukas.run>
2024-03-26 23:25:12 +0100

    Improve layout.
    - make height work
    - add back-button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,23 +18,28 @@
 	});
 </script>
 
-<!-- only show bar if we're not at /login -->
-{#if $page.route.id !== '/login'}
-	<div class="absolute left-2 pt-6 pl-4 flex flex-row">
-		<ThemeSwitch />
-		<!-- only show back-arrow if we're not at charts -->
-		{#if !$page.route.id?.startsWith('/charts')}
-			<a class="ml-6" href=".."><LeftArrow /></a>
-		{/if}
-	</div>
+<!-- this div makes sure that children respect parent's boundaries when using height: 100%-->
+<div class="flex flex-col h-full">
+	<!-- only show bar if we're not at /login -->
+	{#if $page.route.id !== '/login'}
+		<div class="flex flex-row justify-between py-6 px-4 w-full">
+			<div class="flex flex-row">
+				<ThemeSwitch />
+				<!-- only show back-arrow if we're not at charts -->
+				{#if !$page.route.id?.startsWith('/charts')}
+					<button class="ml-6" on:click={() => history.back()}><LeftArrow /></button>
+				{/if}
+			</div>
 
-	<div class="absolute right-2 pt-6 pr-4 flex">
-		<a href="/users"> <UserIcon /> </a>
-		<a class="ml-6" href="/logs"> <Info /> </a>
-		<button class="ml-6" on:click={logout}>
-			<Logout />
-		</button>
-	</div>
-{/if}
+			<div class="flex flex-row">
+				<a href="/users"> <UserIcon /> </a>
+				<a class="ml-6" href="/logs"> <Info /> </a>
+				<button class="ml-6" on:click={logout}>
+					<Logout />
+				</button>
+			</div>
+		</div>
+	{/if}
 
-<slot />
+	<slot />
+</div>
