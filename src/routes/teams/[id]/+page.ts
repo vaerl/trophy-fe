cd2177db9e794d102a9d3d46cff2090b2e3818c6
vaerl <me@lukas.run>
2024-03-29 22:55:12 +0100

    Redirect to /teams if the team can't be found.
    This makes sense if something's been deleted but we've tried to access it.

--- src/routes/teams/[id]/+page.ts
+++ src/routes/teams/[id]/+page.ts
@@ -1,10 +1,17 @@
 import type { Outcome, Team } from '$lib/model.js';
+import { redirect } from '@sveltejs/kit';
 
 export async function load({ fetch, params }) {
 	const baseUrl: string = import.meta.env.VITE_BACKEND_URL;
 
 	const teamRes = await fetch(`${baseUrl}/teams/${params.id}`, { credentials: 'include' });
-	const team: Team = await teamRes.json();
+	const team: Team | { error: string } = await teamRes.json();
+
+	if ('error' in team) {
+		// redirect back to /teams if we couldn't GET the team,
+		// f.e. when using history after deletion
+		redirect(302, '/teams');
+	}
 
 	const outcomeRes = await fetch(`${baseUrl}/outcomes/teams/${params.id}`, {
 		credentials: 'include'
