458bb984f86db3949ebe0ecac732afd23b27ae2b
vaerl <me@lukas.run>
2024-03-29 20:59:42 +0100

    Implement saving of outcomes.

---
+++ src/routes/teams/[id]/+page.server.ts
@@ -0,0 +1,44 @@
+import { TeamGender, type CreateTeam, type Team, type Outcome } from '$lib/model';
+import { fail, redirect } from '@sveltejs/kit';
+
+export const actions = {
+	default: async (event) => {
+		let formData = await event.request.formData();
+
+		let data = formData.get('data');
+		if (!data) {
+			return fail(400, { field: 'Data', missing: true });
+		}
+
+		let outcome = formData.get('outcome');
+		if (!outcome) {
+			return fail(400, { field: 'Outcome', missing: true });
+		}
+
+		let cookie = event.cookies.get('session');
+		if (!cookie) {
+			return fail(400, { unauthorized: true });
+		}
+
+		const baseUrl: string = import.meta.env.VITE_BACKEND_URL;
+		let updatedOutcome: Outcome = {
+			...JSON.parse(outcome.toString()),
+			data
+		};
+		let res = await event.fetch(`${baseUrl}/outcomes`, {
+			method: 'PUT',
+			headers: {
+				// requests won't work without this
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(updatedOutcome)
+		});
+
+		if (res.status != 200) {
+			return fail!(422, { miscellaneous: true, detail: await res.text() });
+		}
+
+		// go back to overview after successful creation
+		return { success: true, outcome: (await res.json()) as Outcome };
+	}
+};
--- src/routes/teams/[id]/+page.svelte
+++ src/routes/teams/[id]/+page.svelte
@@ -1,44 +1,110 @@
 <script lang="ts">
+	import { applyAction, enhance } from '$app/forms';
+	import { invalidateAll } from '$app/navigation';
+	import { GameKind, MessageType, type Outcome } from '$lib/model';
+	import { messageStore } from '$lib/stores.js';
+	import { isEscapeKeyEvent } from '$lib/util';
 	import { DataHandler, Datatable, Th, ThFilter } from '@vincjo/datatables';
 
 	export let data;
+	export let form;
 
-	const openOutcomeHandler = new DataHandler(
-		data.outcomes.filter((o) => !o.data),
+	$: team = data.team;
+	$: outcomes = data.outcomes;
+
+	let modalOutcome: Outcome | null = null;
+	let modalContent: HTMLFormElement;
+
+	$: openOutcomeHandler = new DataHandler(
+		outcomes.filter((o) => !o.data),
 		{ rowsPerPage: 10 }
 	);
-	const openOutcomes = openOutcomeHandler.getRows();
+	$: openOutcomes = openOutcomeHandler.getRows();
 
-	const doneOutcomeHandler = new DataHandler(
-		data.outcomes.filter((o) => o.data),
+	$: doneOutcomeHandler = new DataHandler(
+		outcomes.filter((o) => o.data),
 		{ rowsPerPage: 10 }
 	);
-	const doneOutcomes = doneOutcomeHandler.getRows();
-	// TODO add click-listener for row on both tables
+	$: doneOutcomes = doneOutcomeHandler.getRows();
+
+	/**
+	 * Close the modal if we click out of it.
+	 * @param e
+	 */
+	function onClickOutside(e: MouseEvent) {
+		if (!modalContent.contains(e.target)) {
+			modalOutcome = null;
+		}
+	}
+
+	/**
+	 * Close the modal when pressing Escape.
+	 * @param event
+	 */
+	function handleKeyDown(event: KeyboardEvent) {
+		if (isEscapeKeyEvent(event)) {
+			modalOutcome = null;
+		}
+	}
+
+	$: {
+		if (form?.missing) {
+			messageStore.set({
+				type: MessageType.Error,
+				message: `Das Feld '${form.field}' muss angegeben werden.`
+			});
+		} else if (form?.unauthorized) {
+			messageStore.set({
+				type: MessageType.Error,
+				message: 'Das Ergebnis konnte nicht gespeichert werden, bitte melde dich erneut an.'
+			});
+		} else if (form?.miscellaneous) {
+			messageStore.set({
+				type: MessageType.Error,
+				message: `Etwas ist schiefgelaufen: ${form.detail}`
+			});
+		} else if (form?.success) {
+			messageStore.set({
+				type: MessageType.Success,
+				message: `Ergebnis für ${form.outcome.game_name} wurde erfolgreich gespeichert.`
+			});
+			// This causes the load-function to run again - this is a bit over the top, we'd only need to
+			// re-load outcomes.
+			invalidateAll();
+		}
+	}
 </script>
+
+<svelte:window on:keydown={handleKeyDown} />
 
 <div class="absolute-center-x left-1/2 pt-6">
 	<div class="tooltip" data-tip="Zum Bearbeiten klicken.">
-		<a href={`/teams/${data.team.id}/edit`}>
+		<a href={`/teams/${team.id}/edit`}>
 			<div class="stats border">
 				<div class="stat place-items-center">
 					<div class="stat-title">Name</div>
-					<div class="stat-value">{data.team.name}</div>
+					<div class="stat-value">{team.name}</div>
 				</div>
 
 				<div class="stat place-items-center">
 					<div class="stat-title">Trophy-ID</div>
-					<div class="stat-value">{data.team.trophy_id}</div>
+					<div class="stat-value">{team.trophy_id}</div>
 				</div>
 
 				<div class="stat place-items-center">
 					<div class="stat-title">Typ</div>
-					<div class="stat-value">{data.team.gender}</div>
+					<div class="stat-value">{team.gender}</div>
 				</div>
 
 				<div class="stat place-items-center">
 					<div class="stat-title">Punkte</div>
-					<div class="stat-value text-primary">{data.team.points}</div>
+					<div class="stat-value text-primary">{team.points}</div>
+				</div>
+				<div class="stat place-items-center">
+					<div class="stat-title">Abgeschlossen</div>
+					<div class="stat-value text-secondary">
+						{Math.round(($doneOutcomes.length / outcomes.length) * 100)}%
+					</div>
 				</div>
 			</div></a
 		>
@@ -64,7 +130,7 @@
 
 				<tbody>
 					{#each $openOutcomes as row}
-						<tr>
+						<tr on:click={() => (modalOutcome = row)} class="cursor-pointer">
 							<td>
 								{row.game_trophy_id}
 							</td>
@@ -99,7 +165,7 @@
 
 				<tbody>
 					{#each $doneOutcomes as row}
-						<tr>
+						<tr on:click={() => (modalOutcome = row)} class="cursor-pointer">
 							<td>
 								{row.game_trophy_id}
 							</td>
@@ -116,3 +182,54 @@
 		</Datatable>
 	</div>
 </div>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+{#if modalOutcome}
+	<dialog id="my_modal_2" class="modal modal-open" on:click={(e) => onClickOutside(e)}>
+		<form
+			method="POST"
+			class="modal-box"
+			bind:this={modalContent}
+			use:enhance={({ formData }) => {
+				formData.append('outcome', JSON.stringify(modalOutcome));
+				return async ({ result }) => {
+					await applyAction(result);
+					modalOutcome = null;
+				};
+			}}
+		>
+			<h3 class="font-bold text-xl text-center pb-6">
+				{#if modalOutcome.data}
+					Ergebnis für
+					<a class="link" href={`/games/${modalOutcome.game_id}`}>{modalOutcome.game_name}</a>
+					bearbeiten
+				{:else}
+					Neues Ergebnis für <a class="link" href={`/games/${modalOutcome.game_id}`}
+						>{modalOutcome.game_name}</a
+					> anlegen
+				{/if}
+			</h3>
+
+			<div class="flex justify-center">
+				{#if modalOutcome.game_kind == GameKind.Points}
+					<input
+						class="input input-bordered"
+						name="data"
+						type="number"
+						placeholder="Punkte"
+						value={modalOutcome.data}
+						required
+					/>
+				{:else}
+					<input class="input input-bordered" name="data" type="text" placeholder="Zeit" required />
+				{/if}
+			</div>
+
+			<div class="modal-action flex flex-row justify-between">
+				<button class="btn" on:click={() => (modalOutcome = null)}>Abbrechen</button>
+				<button class="btn btn-neutral">Speichern</button>
+			</div>
+		</form>
+	</dialog>
+{/if}
