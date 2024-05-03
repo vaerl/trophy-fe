db453f42a4fb781a51689078609c11f8a0556dbf
Lukas Müller <me@lukas.run>
2024-05-03 22:51:33 +0200

    Remove nesting for overview.

---
+++ src/routes/overview/+layout.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<!-- if this ever breaks, nest this and slot under a parent-div -->
+<h1 class="text-6xl font-extrabold absolute-center-x pt-6">Übersicht</h1>
+
+<slot />
