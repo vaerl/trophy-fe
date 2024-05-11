1c1eb527056ec9de01ca54cbfedc7d00b0a77d58
Lukas Müller <me@lukas.run>
2024-05-11 23:06:57 +0200

    Re-Implement user-details.

--- src/routes/users/[id]/+page.svelte
+++ src/routes/users/[id]/+page.svelte
@@ -1,13 +1,59 @@
 <script lang="ts">
-	import Details from '../../../components/details/Details.svelte';
+	import Edit from '../../../components/icons/Edit.svelte';
+	import Delete from '../../../components/icons/Delete.svelte';
+	import { messageStore } from '$lib/stores';
+	import { MessageType } from '$lib/model';
+
+	export let data;
+	export let form;
+
+	$: {
+		if (form?.unauthorized) {
+			messageStore.set({
+				type: MessageType.Error,
+				message: 'Das Ergebnis konnte nicht gespeichert werden, bitte melde dich erneut an.'
+			});
+		} else if (form?.miscellaneous) {
+			messageStore.set({
+				type: MessageType.Error,
+				message: `Etwas ist schiefgelaufen: ${form.detail}`
+			});
+		}
+	}
 </script>
 
-<Details
-	columns={[
-		{ id: 'id', name: 'ID' },
-		{ id: 'name', name: 'Nutzername' },
-		{ id: 'password', name: 'Passwort' },
-		{ id: 'role', name: 'Rolle' }
-	]}
-	type="user"
-/>
+<!-- this is kinda hacky, but works -->
+<div class="absolute right-0 top-0 py-6 mr-28 flex flex-row">
+	<a href={`/users/${data.user.id}/edit`} class="ml-6"><Edit /></a>
+	<form method="POST" action="?/delete">
+		<button class="ml-6"><Delete /></button>
+	</form>
+</div>
+
+<div class="absolute-center-x absolute-center-y">
+	<div class="tooltip" data-tip="Zum Bearbeiten klicken.">
+		<a href={`/users/${data.user.id}/edit`}>
+			<div class="stats border">
+				<div class="stat place-items-center">
+					<div class="stat-title">Name</div>
+					<div class="stat-value">{data.user.name}</div>
+				</div>
+
+				<div class="stat place-items-center">
+					<div class="stat-title">Rolle</div>
+					<div class="stat-value">{data.user.role}</div>
+				</div>
+
+				<!-- link to the game if the user has one -->
+				{#if data.user.game_id}
+					<a href={`/games/${data.user.game_id}`}>
+						<div class="stat place-items-center">
+							<div class="stat-title">Spiel</div>
+							<div class="stat-value">{data.user.game_name}</div>
+						</div>
+					</a>
+				{/if}
+			</div>
+		</a>
+	</div>
+</div>
