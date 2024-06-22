06aba8ec69cea2ef0675a34d225317c9b566b77d
Lukas Müller <me@lukas.run>
2024-06-22 15:47:22 +0200

    Try a basic theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     extend: {},
   },
   daisyui: {
-    themes: ["light"],
+    themes: [
+      {
+        trophy: {
+          "primary": "#414535",
+          "secondary": "#6A7FDB",
+          "accent": "#eec6ca",
+          "neutral": "#EDEDF4",
+          "info": "#C6D4FF",
+          "success": "#009485",
+          "warning": "#eac435",
+          "error": "#ff5724",
+          "base-100": "#ffffff",
+          "neutral-content": "#160016",
+        },
+      },
+    ],
   },
   plugins: [
     require('daisyui')
