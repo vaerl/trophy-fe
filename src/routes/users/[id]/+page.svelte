d5eac720d693da16db2958e26bc6e4db3a046041
Lukas Müller <me@lukas.run>
2024-06-22 16:30:20 +0200

    Implement confirmation-dialog for users.

--- src/routes/users/[id]/+page.svelte
+++ src/routes/users/[id]/+page.svelte
@@ -6,6 +6,10 @@
 	import { goto } from '$app/navigation';
 
 	export let data;
+	let showDeletion = false;
+	let nameInput: string;
+	let modalContent: HTMLFormElement;
+
 	async function handleDelete(event: any) {
 		const baseUrl: string = import.meta.env.VITE_BACKEND_URL;
 		let res = await fetch(`${baseUrl}/users/${data.user.id}`, {
@@ -33,14 +37,30 @@
 		});
 		goto(`/users`);
 	}
+
+	/**
+	 * Close the modal if we click out of it.
+	 * @param e
+	 */
+	function onClickOutside(e: any) {
+		if (!modalContent.contains(e.target)) {
+			closeDeletion();
+		}
+	}
+
+	/**
+	 * Hide the modal and reset the value of the input.
+	 */
+	function closeDeletion() {
+		showDeletion = false;
+		nameInput = '';
+	}
 </script>
 
 <!-- this is kinda hacky, but works -->
 <div class="absolute right-0 top-0 py-6 mr-28 flex flex-row">
 	<a href={`/users/${data.user.id}/edit?year=${$yearStore}`} class="ml-6"><Edit /></a>
-	<form on:submit|preventDefault={handleDelete}>
-		<button class="ml-6"><Delete /></button>
-	</form>
+	<button class="ml-6" on:click={() => (showDeletion = true)}><Delete /></button>
 </div>
 
 <div class="absolute-center-x absolute-center-y">
@@ -70,3 +90,39 @@
 		</a>
 	</div>
 </div>
+
+{#if showDeletion}
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+	<dialog class="modal modal-open" on:click={(e) => onClickOutside(e)}>
+		<form id="confirmation-form" class="modal-box" bind:this={modalContent}>
+			<button
+				class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+				on:click={closeDeletion}>✕</button
+			>
+
+			<h1 class="font-bold text-xl text-center pb-2">
+				Nutzer "{data.user.name}" wirklich löschen?
+			</h1>
+			<p class="text-center pb-6">Bitte gib den Namen des Nutzers ein, um es zu löschen.</p>
+			<input
+				class="input input-bordered w-full"
+				name="input"
+				type="text"
+				required
+				bind:value={nameInput}
+			/>
+
+			<div class="modal-action flex flex-row justify-between">
+				<button class="btn btn-secondary" on:click|preventDefault={closeDeletion}>
+					Abbrechen
+				</button>
+				<button
+					class="btn btn-primary"
+					class:btn-disabled={data.user.name !== nameInput}
+					on:click|preventDefault={handleDelete}>Löschen</button
+				>
+			</div>
+		</form>
+	</dialog>
+{/if}
