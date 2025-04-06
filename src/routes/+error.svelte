a7bc08d501a5915f2030c197590f06e3b3542133
Lukas Müller <me@lukas.run>
2025-04-06 10:51:31 +0200

    Switch to new page-store.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,11 +1,11 @@
 <script>
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { yearStore } from '$lib/stores';
 </script>
 
 <div class="absolute-center-x w-full text-center">
 	<h1 class="font-extrabold text-4xl pb-4">
-		{$page.status}: {$page.error?.message}
+		{page.status}: {page.error?.message}
 	</h1>
 	<a href="/overview/pie?year={$yearStore}"
 		><h2 class="font-bold link text-2xl">Zurück zur Übersicht</h2></a
