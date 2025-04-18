5d28647dc8bb668da3c8684c2237f702e1a4546c
Lukas Müller <me@lukas.run>
2025-04-18 12:52:23 +0200

    Return promise for /teams.

--- src/routes/teams/+page.ts
+++ src/routes/teams/+page.ts
@@ -1,15 +1,17 @@
 import type { Team } from '$lib/model';
 import { getYear } from '$lib/util';
+import type { PageLoad } from './$types.js';
 
-export async function load({ fetch }) {
+export const load: PageLoad = ({ fetch }) => {
 	const baseUrl: string = import.meta.env.VITE_BACKEND_URL;
 	let year = getYear();
 	let params = `?year=${year}`;
 
-	const teamsRes = await fetch(`${baseUrl}/teams${params}`, { credentials: 'include' });
-	const teams: Team[] = await teamsRes.json();
+	const teams: Promise<Team[]> = fetch(`${baseUrl}/teams${params}`, {
+		credentials: 'include'
+	}).then((res) => res.json());
 
 	return {
 		teams
 	};
-}
+};
