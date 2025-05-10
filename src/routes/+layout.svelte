2f48eb5d153901ddad93e11df44d0fff5fac36e8
Lukas Müller <me@lukas.run>
2025-05-11 00:28:27 +0200

    Fix focus.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,12 +11,14 @@
 		type Team,
 		type User
 	} from '$lib/model';
-	import Fuse, { type FuseResult } from 'fuse.js';
+	import Fuse from 'fuse.js';
 	import { goto } from '$app/navigation';
 	import hotkeys from 'hotkeys-js';
 	import type { LayoutProps } from './$types';
+	import Loader from '../components/blocks/Loader.svelte';
 
 	let { data, children }: LayoutProps = $props();
+	let { games, users, teams } = $derived(data);
 	let switcherModalId = 'switcher-modal';
 	type SwitcherItem = {
 		displayName: DisplayName;
@@ -29,22 +31,29 @@
 	let searchValue = $state('');
 	let selectedIndex = $state(0);
 
-	let mixedObjects = $derived([
-		...data.teams.map((t: Team) => ({ ...t, displayName: 'Team', link: '/teams' })),
-		...data.games.map((t: Game) => ({ ...t, displayName: 'Spiel', link: '/games' })),
-		...data.users.map((t: User) => ({ ...t, displayName: 'Nutzer', link: '/users' }))
-	] as SwitcherItem[]);
+	// TODO this will be much nicer once https://github.com/sveltejs/svelte/pull/15844 is merged
+	let mixedObjects = $derived.by(() =>
+		Promise.all([games, users, teams]).then(
+			([games, users, teams]) =>
+				[
+					...teams.map((t: Team) => ({ ...t, displayName: 'Team', link: '/teams' })),
+					...games.map((t: Game) => ({ ...t, displayName: 'Spiel', link: '/games' })),
+					...users.map((t: User) => ({ ...t, displayName: 'Nutzer', link: '/users' }))
+				] as SwitcherItem[]
+		)
+	);
 	const options = {
 		keys: ['name', 'trophy_id', 'type'],
 		threshold: 0.3
 	};
-	let fuse = $derived(new Fuse(mixedObjects, options));
-	let searchResults = $derived.by(() => {
+	let fuse = $derived.by(() => mixedObjects.then((o) => new Fuse(o, options)));
+	let searchResults = $derived.by(async () => {
 		if (searchValue != '') {
 			// only display the first 10 results
-			return fuse.search(searchValue).slice(0, 10);
+			const f = await fuse;
+			return f.search(searchValue).slice(0, 10);
 		}
-		return [];
+		return Promise.resolve([]);
 	});
 
 	let toast: Message | undefined = $state(undefined);
@@ -75,27 +84,29 @@
 				searchValue = '';
 				break;
 			case 'enter':
-				let result: FuseResult<SwitcherItem> | undefined = searchResults[selectedIndex];
+				let result = searchResults.then((results) => results[selectedIndex]);
 				// reset the switcher after capturing the result
 				(document.getElementById(switcherModalId) as HTMLDialogElement).close();
 				searchValue = '';
 				if (result) {
-					goto(`${result.item.link}/${result.item.id}`);
+					result.then((r) => goto(`${r.item.link}/${r.item.id}`));
 				}
 				break;
 			case 'up':
 				if (selectedIndex == 0) {
-					selectedIndex = searchResults.length - 1;
+					searchResults.then((results) => results.length).then((len) => (selectedIndex = len - 1));
 				} else {
 					selectedIndex--;
 				}
 				break;
 			case 'down':
-				if (selectedIndex + 1 == searchResults.length) {
-					selectedIndex = 0;
-				} else {
-					selectedIndex++;
-				}
+				searchResults.then((results) => {
+					if (selectedIndex + 1 == results.length) {
+						selectedIndex = 0;
+					} else {
+						selectedIndex++;
+					}
+				});
 				break;
 		}
 	});
@@ -103,7 +114,7 @@
 	onDestroy(unsub);
 </script>
 
-{@render children?.()}
+{@render children()}
 
 {#if toast}
 	<div class="absolute bottom-4 right-4 flex justify-end">
@@ -125,9 +136,14 @@
 	</div>
 {/if}
 
-<!-- svelte-ignore a11y_click_events_have_key_events -->
-<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
-<dialog class="modal" id={switcherModalId}>
+<!-- NOTE focusing is extremely hacky - neither autofocus not ontransitioned work on their own.
+We focus the input initially using ontransitioned and keep focus with autofocus.
+-->
+<dialog
+	class="modal"
+	id={switcherModalId}
+	ontransitionend={() => (document.getElementById('switcher-input') as HTMLInputElement).focus()}
+>
 	<div class="modal-box">
 		<form method="dialog">
 			<button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
@@ -135,47 +151,50 @@
 		<form>
 			<h1 class="font-bold text-xl text-center pb-2">Switcher</h1>
 
-			<!-- svelte-ignore a11y_autofocus -->
-			<input
-				class="input input-bordered w-full mb-4"
-				name="switcher"
-				type="text"
-				autofocus
-				bind:value={searchValue}
-			/>
-
-			{#if searchValue != '' && searchResults && searchResults.length == 0}
-				<p>
-					Keine Treffer für '{searchValue}'.
-				</p>
-			{:else}
-				<ul class="menu rounded-box w-full p-0 gap-1">
-					{#each searchResults as result}
-						<li class="w-full">
-							<a
-								href={`${result.item.link}/${result.item.id}`}
-								onclick={() => {
-									(document.getElementById(switcherModalId) as HTMLDialogElement).close();
-									searchValue = '';
-								}}
-								class="flex flex-row justify-between w-full font-bold"
-								class:bg-primary={result.refIndex == searchResults[selectedIndex].refIndex}
-								class:text-white={result.refIndex == searchResults[selectedIndex].refIndex}
-							>
-								<p>
-									{result.item.name}
-									{#if result.item.link !== '/users'}
-										- {result.item.trophy_id}
-									{/if}
-								</p>
-								<p>
-									{result.item.displayName}
-								</p>
-							</a>
-						</li>
-					{/each}
-				</ul>
-			{/if}
+			{#await searchResults}
+				<Loader></Loader>
+			{:then results}
+				<!-- svelte-ignore a11y_autofocus -->
+				<input
+					class="input input-bordered w-full mb-4"
+					id="switcher-input"
+					type="text"
+					bind:value={searchValue}
+					autofocus
+				/>
+				{#if searchValue != '' && results && results.length == 0}
+					<p>
+						Keine Treffer für '{searchValue}'.
+					</p>
+				{:else}
+					<ul class="menu rounded-box w-full p-0 gap-1">
+						{#each results as result}
+							<li class="w-full">
+								<a
+									href={`${result.item.link}/${result.item.id}`}
+									onclick={() => {
+										(document.getElementById(switcherModalId) as HTMLDialogElement).close();
+										searchValue = '';
+									}}
+									class="flex flex-row justify-between w-full font-bold"
+									class:bg-primary={result.refIndex == results[selectedIndex].refIndex}
+									class:text-white={result.refIndex == results[selectedIndex].refIndex}
+								>
+									<p>
+										{result.item.name}
+										{#if result.item.link !== '/users'}
+											- {result.item.trophy_id}
+										{/if}
+									</p>
+									<p>
+										{result.item.displayName}
+									</p>
+								</a>
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			{/await}
 		</form>
 	</div>
 	<form method="dialog" class="modal-backdrop">
