65f1a16e29610fd3fb62d77bae48b6acbb87b7d1
Lukas Müller <me@lukas.run>
2026-06-19 20:22:58 +0200

    Fix ItemTable.

--- src/lib/components/table/ItemsTable.svelte
+++ src/lib/components/table/ItemsTable.svelte
@@ -61,14 +61,10 @@
 
 <svelte:window onkeydown={keyDown} onkeyup={keyUp} />
 
-<div class="flex flex-col h-screen">
+<div class="flex flex-col h-full w-full">
 	<Navbar title={typeName}>
 		{#snippet right()}
-			{#await linkPrefix}
-				<Loader></Loader>
-			{:then linkPrefix}
-				<a href={`/${linkPrefix}/create`}> <Plus /> </a>
-			{/await}
+			<a href={`/${linkPrefix}/create`}> <Plus /> </a>
 		{/snippet}
 	</Navbar>
 	{#await table}
@@ -76,52 +72,68 @@
 			<Loader></Loader>
 		</div>
 	{:then table}
-		<div class="overflow-hidden">
-			<Datatable basic {table}>
-				<table>
-					<thead>
-						<tr>
-							<ThSort {table} field="trophy_id" direction="asc">Trophy-ID</ThSort>
-							<ThSort {table} field="name">Name</ThSort>
-							{#if routeIncludesTeams}
-								<ThSort {table} field={'gender' as Field<Team>}>Typ</ThSort>
-								<ThSort {table} field={'points' as Field<Team>}>Punkte</ThSort>
-							{:else}
-								<ThFilter {table} field={'kind' as Field<Game>} />
-							{/if}
-						</tr>
-						<tr>
-							<ThFilter {table} field="trophy_id" />
-							<ThFilter {table} field="name" />
-							{#if routeIncludesTeams}
-								<ThFilter {table} field={'gender' as Field<Team>} />
-								<ThFilter {table} field={'points' as Field<Team>} />
-							{:else}
-								<ThFilter {table} field={'kind' as Field<Game>} />
-							{/if}
-						</tr>
-					</thead>
+		<Datatable basic {table}>
+			<table>
+				<thead>
+					<tr>
+						<ThSort {table} field="trophy_id" direction="asc">Trophy-ID</ThSort>
+						<ThSort {table} field="name">Name</ThSort>
+						{#if routeIncludesTeams}
+							<ThSort {table} field={'gender' as Field<Team>}>Typ</ThSort>
+							<ThSort {table} field={'points' as Field<Team>}>Punkte</ThSort>
+						{:else}
+							<ThSort {table} field={'kind' as Field<Game>}>Punktart</ThSort>
+						{/if}
+					</tr>
+					<tr>
+						<ThFilter {table} field="trophy_id" />
+						<ThFilter {table} field="name" />
+						{#if routeIncludesTeams}
+							<ThFilter {table} field={'gender' as Field<Team>} />
+							<ThFilter {table} field={'points' as Field<Team>} />
+						{:else}
+							<ThFilter {table} field={'kind' as Field<Game>} />
+						{/if}
+					</tr>
+				</thead>
 
-					<tbody>
-						{#each table.rows as row}
-							<!-- even though I'd like to use <a>, only this will make sure the entire row is clickable -->
-							<tr onclick={() => goto(`/${linkPrefix}/${row.id}`)} class="cursor-pointer">
-								<td>
-									{row.trophy_id}
-								</td>
-								<td>{row.name}</td>
-								<!-- using isTeam gives us the type-assertion -->
-								{#if isTeam(row)}
-									<td>{row.gender}</td>
-									<td>{row.points}</td>
-								{:else}
-									<td>{row.kind}</td>
-								{/if}
-							</tr>
-						{/each}
-					</tbody>
-				</table>
-			</Datatable>
-		</div>
+				<tbody>
+					{#each table.rows as row}
+						<!-- even though I'd like to use <a>, only this will make sure the entire row is clickable -->
+						<tr onclick={() => goto(`/${linkPrefix}/${row.id}`)} class="cursor-pointer">
+							<td>
+								{row.trophy_id}
+							</td>
+							<td>{row.name}</td>
+							<!-- using isTeam gives us the type-assertion -->
+							{#if isTeam(row)}
+								<td>{row.gender}</td>
+								<td>{row.points}</td>
+							{:else}
+								<td>{row.kind}</td>
+							{/if}
+						</tr>
+					{/each}
+				</tbody>
+			</table>
+		</Datatable>
 	{/await}
 </div>
+
+<style>
+	/* Improve height of footer. */
+	:global(footer) {
+		height: auto !important;
+		min-height: 2rem !important;
+		padding-top: 0.5rem !important;
+		padding-bottom: 0rem !important;
+		margin-bottom: 0.5rem;
+		display: flex !important;
+	}
+
+	/* remove the divider-classes - DaisyUI causes an issue here. */
+	:global(footer.divider::before),
+	:global(footer.divider::after) {
+		display: none !important;
+	}
+</style>
